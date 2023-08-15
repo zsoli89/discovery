255dd7f3316423be84969b900fd9bd6001684fc9
zsoli89 <jakabzsoli89@gmail.com>
2023-08-15 14:40:26 +0200

    git init discovery server

---
+++ src/test/java/hu/webuni/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.webuni.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
